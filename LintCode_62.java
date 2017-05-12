0d5f05cca27068d2047a1a50c71066b87bcbcf9c
misspink1011 <xuanwang1118@gmail.com>
2017-05-12 10:55:17 -0400

    Search in Rotated Sorted Array

---
+++ LintCode_62.java
@@ -0,0 +1,63 @@
+public class LintCode_62 {
+    /** 
+     *@param A : an integer rotated sorted array
+     *@param target :  an integer to be searched
+     *return : an integer
+     */
+    public int search(int[] A, int target) {
+        // write your code here
+        if(A == null || A.length == 0){
+            return -1;
+        }
+        
+        int minIndex = findMin(A);
+        int start, end, mid;
+        if(target <= A[A.length - 1]){
+            start = minIndex;
+            end = A.length - 1;
+        }
+        else{
+            start = 0;
+            end = minIndex - 1;
+        }
+        
+        while(start + 1 < end){
+            mid = start + (end - start) / 2;
+            if(A[mid] == target){
+                return mid;
+            }
+            if(A[mid] < target){
+                start = mid;
+            }
+            else{
+                end = mid;
+            }
+        }
+        
+        if(A[start] == target){
+            return start;
+        }
+        if(A[end] == target){
+            return end;
+        }
+        return -1;
+    }
+    
+    public int findMin(int[] A) {
+        int start = 0, end = A.length - 1;
+        while(start + 1  < end){
+            int mid = start + (end - start) / 2;
+            if(A[mid] <= A[A.length - 1]){
+                end = mid;
+            }
+            else{
+                start = mid;
+            }
+        }
+        if(A[start] < A[end]){
+            return start;
+        }
+        return end;
+    }
+
+}
