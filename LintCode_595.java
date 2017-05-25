6b1f77d470afa784b4eaac2c7cead45559317521
misspink1011 <xuanwang1118@gmail.com>
2017-05-25 10:40:00 -0400

    Binary Tree Longest Consecutive Sequence I

--- LintCode_595.java
+++ LintCode_595.java
@@ -33,3 +33,54 @@ public class LintCode_595 {
         return curlongth;
     }
 }
+
+public class LintCode_595_2 {
+    private class ResultType {
+        int maxInSubtree;
+        int maxFromRoot;
+        public ResultType(int maxInSubtree, int maxFromRoot) {
+            this.maxInSubtree = maxInSubtree;
+            this.maxFromRoot = maxFromRoot;
+        }
+    }
+    /**
+     * @param root the root of binary tree
+     * @return the length of the longest consecutive sequence path
+     */
+    public int longestConsecutive(TreeNode root) {
+        return helper(root).maxInSubtree;
+    }
+    
+    private ResultType helper(TreeNode root) {
+        if (root == null) {
+            return new ResultType(0, 0);
+        }
+        
+        ResultType left = helper(root.left);
+        ResultType right = helper(root.right);
+        
+        // 1 is the root itself.
+        ResultType result = new ResultType(0, 1);
+        
+        if (root.left != null && root.val + 1 == root.left.val) {
+            result.maxFromRoot = Math.max(
+                result.maxFromRoot,
+                left.maxFromRoot + 1
+            );
+        }
+        
+        if (root.right != null && root.val + 1 == root.right.val) {
+            result.maxFromRoot = Math.max(
+                result.maxFromRoot,
+                right.maxFromRoot + 1
+            );
+        }
+        
+        result.maxInSubtree = Math.max(
+            result.maxFromRoot,
+            Math.max(left.maxInSubtree, right.maxInSubtree)
+        );
+        
+        return result;
+    }
+}
