61702247eccc2bb6b9086585637602a9740e9335
misspink1011 <xuanwang1118@gmail.com>
2017-05-30 15:50:39 -0400

    Binary Tree Path Sum

--- LintCode_376.java
+++ LintCode_376.java
@@ -39,3 +39,42 @@ public class LintCode_376 {
         return paths;
     }
 }
+
+public class LintCode_376_2 {
+    /**
+     * @param root the root of binary tree
+     * @param target an integer
+     * @return all valid paths
+     */
+    public List<List<Integer>> binaryTreePathSum(TreeNode root, int target) {
+        // Write your code here
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if(root == null){
+            return result;
+        }
+        List<Integer> path = new ArrayList<>();
+        path.add(root.val);
+        helper(root, path, result, root.val, target);
+        return result;
+    }
+    
+    private void helper(TreeNode root, List<Integer> path, List<List<Integer>> result, int sum, int target){
+        if(root.left == null && root.right == null){
+            if(sum == target){
+                result.add(new ArrayList<Integer>(path));
+            }
+            return;
+        }
+        
+        if(root.left != null){
+            path.add(root.left.val);
+            helper(root.left, path, result, sum + root.left.val, target);
+            path.remove(path.size() - 1);
+        }
+        if(root.right != null){
+            path.add(root.right.val);
+            helper(root.right, path, result, sum + root.right.val, target);
+            path.remove(path.size() - 1);
+        }
+    }
+}
