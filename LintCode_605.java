8f6834cb6b94627898c78e18bef007e6239a8b4f
misspink1011 <xuanwang1118@gmail.com>
2017-06-06 15:16:08 -0400

    Course Schedule II

--- LintCode_605.java
+++ LintCode_605.java
@@ -58,3 +58,63 @@ public class LintCode_605 {
         return true;
     }
 }
+
+
+
+public class LintCode_605_2 {
+    /**
+     * @param org a permutation of the integers from 1 to n
+     * @param seqs a list of sequences
+     * @return true if it can be reconstructed only one or false
+     */
+    public boolean sequenceReconstruction(int[] org, int[][] seqs) {
+        // Write your code here
+        Map<Integer, ArrayList<Integer>> map = new HashMap<>();
+        Map<Integer, Integer> indegree = new HashMap<>();
+        for(int num : org){
+            map.put(num, new ArrayList<Integer>());
+            indegree.put(num, 0);
+        }
+        
+        int n = org.length;
+        int lengthSum = 0;
+        for(int[] seq : seqs){
+            lengthSum += seq.length;
+            if(seq.length >= 1 && (seq[0] < 1 || seq[0] > n)){
+                return false;
+            }
+            for(int i = 1; i < seq.length; i++){
+                if(seq[i] < 0 || seq[i] > n){
+                    return false;
+                }
+                map.get(seq[i - 1]).add(seq[i]);
+                indegree.put(seq[i], indegree.get(seq[i]) + 1);
+            }
+        }
+        
+        if(lengthSum < n){
+            return false;
+        }
+        
+        Queue<Integer> queue = new LinkedList<>();
+        for(int num : org){
+            if(indegree.get(num) == 0){
+                queue.offer(num);
+            }
+        }
+        
+        int count = 0;
+        while(queue.size() == 1){
+            int first = (int)queue.poll();
+            count++;
+            for(int course : map.get(first)){
+                indegree.put(course, indegree.get(course) - 1);
+                if(indegree.get(course) == 0){
+                    queue.offer(course);
+                }
+            }
+        }
+        
+        return count == n;
+    }
+}
