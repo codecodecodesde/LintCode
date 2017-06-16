6cb4f543b3f8bcc298bd6cb955659294ff6fc595
misspink1011 <xuanwang1118@gmail.com>
2017-06-16 15:16:15 -0400

    String Permutation

--- LintCode_211.java
+++ LintCode_211.java
@@ -26,3 +26,39 @@ public class LintCode_211 {
         return true;
     }
 }
+
+public class LintCode_211_2 {
+    /**
+     * @param A a string
+     * @param B a string
+     * @return a boolean
+     */
+    public boolean stringPermutation(String A, String B) {
+        // Write your code here
+        Map<Character, Integer> hashmap =new HashMap<>();
+        for(int i = 0; i < A.length(); i++){
+            if(hashmap.containsKey(A.charAt(i))){
+                hashmap.put(A.charAt(i), hashmap.get(A.charAt(i)) + 1);
+            }
+            else{
+                hashmap.put(A.charAt(i), 1);
+            }
+        }
+        
+        for(int i  = 0; i < B.length(); i++){
+            if(hashmap.containsKey(B.charAt(i))){
+                hashmap.put(B.charAt(i), hashmap.get(B.charAt(i)) - 1);
+            }
+            else{
+                return false;
+            }
+        }
+        
+        for(Object value : hashmap.values()){
+            if((int)value != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
