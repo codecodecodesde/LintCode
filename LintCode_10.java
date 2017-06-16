080cb010a9b20328877a11239062035697478450
misspink1011 <xuanwang1118@gmail.com>
2017-06-16 15:29:07 -0400

    String Permutation II

--- LintCode_10.java
+++ LintCode_10.java
@@ -43,3 +43,59 @@ public class LintCode_10 {
         }
     }
 }
+
+public class LintCode_10_2{
+    /**
+     * @param str a string
+     * @return all permutations
+     */
+    public List<String> stringPermutation2(String str) {
+        // Write your code here
+        List<String> result = new ArrayList<String>();
+        char[] s = str.toCharArray();
+        Arrays.sort(s);
+        result.add(String.valueOf(s));
+        while ((s = nextPermutation(s)) != null) {
+            result.add(String.valueOf(s));
+        }
+        return result;
+    }
+    
+    private char[] nextPermutation(char[] s){
+        int smaller = -1;
+        for(int i = s.length - 2; i >= 0; i--){
+            if(s[i] < s[i + 1]){
+                smaller = i;
+                break;
+            }
+        }
+        
+        if(smaller == -1){
+            return null;
+        }
+        
+        int bigger = -1;
+        for(int i = s.length - 1; i  > smaller; i--){
+            if(s[i] > s[smaller]){
+                bigger = i;
+                break;
+            }
+        }
+        
+        swap(s, smaller, bigger);
+        swapList(s, smaller + 1, s.length - 1);
+        return s;
+    }
+    
+    private void swap(char[] s, int start, int end){
+        char tmp = s[start];
+        s[start] = s[end];
+        s[end] = tmp;
+    }
+    
+    private void swapList(char[] s, int start, int end){
+        for(int i = start, j = end; i < j; i++, j--){
+            swap(s, i, j);
+        }
+    }
+}
