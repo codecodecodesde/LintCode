7ec67f485fb58680bf930d95e2d228329d00ea92
misspink1011 <xuanwang1118@gmail.com>
2017-06-20 14:47:09 -0400

    Linked List Cycle

---
+++ LintCode_102.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class LintCode_102 {
+    /**
+     * @param head: The first node of linked list.
+     * @return: True if it has a cycle, or false
+     */
+    public boolean hasCycle(ListNode head) {  
+        // write your code here
+        if(head == null || head.next == null){
+            return false;
+        }
+        ListNode slow = head, fast = head.next;
+        while(slow != fast){
+            if(fast == null || fast.next == null){
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
