2b8a22e74f85ac88164a9aeea862c335feca694d
misspink1011 <xuanwang1118@gmail.com>
2017-06-20 14:48:04 -0400

    Linked List Cycle

--- LintCode_102.java
+++ LintCode_102.java
@@ -30,3 +30,25 @@ public class LintCode_102 {
         return true;
     }
 }
+
+public class LintCode_102_2 {
+    /**
+     * @param head: The first node of linked list.
+     * @return: True if it has a cycle, or false
+     */
+    public boolean hasCycle(ListNode head) {  
+        // write your code here
+        if(head == null){
+            return false;
+        }
+        ListNode slow = head, fast = head;
+        while(slow != null && fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
