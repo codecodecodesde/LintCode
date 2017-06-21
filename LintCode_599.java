f2e347756be4b5a9db36d0938b14a58667535f0d
misspink1011 <xuanwang1118@gmail.com>
2017-06-20 22:00:45 -0400

    Insert into a Cyclic Sorted List

---
+++ LintCode_599.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class LintCode_599 {
+    /**
+     * @param node a list node in the list
+     * @param x an integer
+     * @return the inserted new list node
+     */
+    public ListNode insert(ListNode node, int x) {
+        // Write your code here
+        if(node == null){
+            node = new ListNode(x);
+            node.next = node;
+            return node;
+        }
+        
+        ListNode cur = node, next = node.next;
+        while(next != node){
+            if(x <= next.val && x >= cur.val){
+                break;
+            }
+            if((next.val < cur.val) && (x < next.val || x > cur.val)){
+                break;
+            }
+            // if(x >= next.val && x <= cur.val){
+            //     break;
+            // }
+            // if((next.val > cur.val) && (x > next.val || x < cur.val)){
+            //     break;
+            // }
+            cur = next;
+            next = next.next;
+        }
+        
+        ListNode newNode = new ListNode(x);
+        newNode.next = next;
+        cur.next = newNode;
+        return newNode;
+    }
+}
