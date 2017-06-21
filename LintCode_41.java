bc6ec77f34f25e4ebfd3c3f1cc41f341b115fd30
misspink1011 <xuanwang1118@gmail.com>
2017-06-21 15:21:40 -0400

    Maximum Subarray I

---
+++ LintCode_41.java
@@ -0,0 +1,20 @@
+public class LintCode_41 {
+    /**
+     * @param nums: A list of integers
+     * @return: A integer indicate the sum of max subarray
+     */
+    public int maxSubArray(int[] nums) {
+        // write your code
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        
+        int max = Integer.MIN_VALUE, sum = 0, minSum = 0;
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+            max = Math.max(max, sum - minSum);
+            minSum = Math.min(sum, minSum);
+        }
+        return max;
+    }
+}
