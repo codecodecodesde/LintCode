b27df742122502b5ef67b5b534c46cd978a3c43d
misspink1011 <xuanwang1118@gmail.com>
2017-06-20 22:43:59 -0400

    Merge Two Sorted Arrays

---
+++ LintCode_6.java
@@ -0,0 +1,33 @@
+class LintCode_6 {
+    /**
+     * @param A and B: sorted integer array A and B.
+     * @return: A new sorted integer array
+     */
+    public int[] mergeSortedArray(int[] A, int[] B) {
+        // Write your code here
+        if(A == null || B == null){
+            return null;
+        }
+        
+        int[] result = new int[A.length + B.length];
+        int i = 0, j = 0;
+        int index = 0;
+        while(i < A.length && j < B.length){
+           if(A[i] <= B[j]){
+               result[index++] = A[i++];
+           }
+           else{
+               result[index++] = B[j++];
+           }
+        }
+        
+        while(i < A.length){
+            result[index++] = A[i++];
+        }
+        
+        while(j < B.length){
+            result[index++] = B[j++];
+        }
+        return result;
+    }
+}
