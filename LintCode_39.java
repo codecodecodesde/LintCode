92a7bb9c7a70c31fbbe75ac68ea8421b9e938b4a
misspink1011 <xuanwang1118@gmail.com>
2017-06-26 12:12:38 -0400

    Recover Rotated Sorted Array

--- LintCode_39.java
+++ LintCode_39.java
@@ -27,3 +27,29 @@ public class LintCode_39 {
         }
     }
 }
+
+public class LintCode_39_2 {
+    /**
+     * @param nums: The rotated sorted array
+     * @return: void
+     */
+    public void recoverRotatedSortedArray(ArrayList<Integer> nums) {
+        // write your code
+        int end = nums.size() - 1;
+        for(int start = 0; start < end; start++){
+            if(nums.get(start) > nums.get(start + 1)){
+                reverse(nums, 0, start);
+                reverse(nums, start + 1, end);
+                reverse(nums, 0, end);
+            }
+        }
+        
+       
+    }
+    
+    private void reverse(ArrayList<Integer> nums, int start, int end){
+        while(start < end){
+            Collections.swap(nums, start++, end--);
+        }
+    }
+}
