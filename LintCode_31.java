6404977a80b0844c021eb3b2b1434d492d07b625
misspink1011 <xuanwang1118@gmail.com>
2017-06-28 14:28:58 -0400

    Partition Array

---
+++ LintCode_31.java
@@ -0,0 +1,32 @@
+public class LintCode_31 {
+	/** 
+     *@param nums: The integer array you should partition
+     *@param k: As description
+     *return: The index after partition
+     */
+    public int partitionArray(int[] nums, int k) {
+	    //write your code here
+	    if(nums == null || nums.length == 0){
+	        return 0;
+	    }
+	    
+	    int left = 0, right = nums.length - 1;
+	    while(left < right){
+	        while(left < right && nums[left] < k){
+	            left++;
+	        }
+	        while(left < right && nums[right] >= k){
+	            right --;
+	        }
+	        
+	        int tmp = nums[left];
+	        nums[left] = nums[right];
+	        nums[right] = tmp;
+	    }
+	    
+	    if(nums[left] < k){
+	        return left + 1;
+	    }
+	    return right;
+    }
+}
