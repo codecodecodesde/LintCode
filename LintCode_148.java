cdab82068fc45dee17cb880e6f3d8a532613e361
misspink1011 <xuanwang1118@gmail.com>
2017-06-28 18:45:13 -0400

    Sort Colors I

---
+++ LintCode_148.java
@@ -0,0 +1,35 @@
+class LintCode_148 {
+    /**
+     * @param nums: A list of integer which is 0, 1 or 2 
+     * @return: nothing
+     */
+    public void sortColors(int[] nums) {
+        // write your code here
+        if(nums == null || nums.length == 0){
+            return;
+        }
+        
+        int left = 0, right = nums.length - 1;
+        int i = 0;
+        while(i <= right){
+            if(nums[i] == 0){
+                swap(nums, i, left);
+                left++;
+                i++;
+            }
+            else if(nums[i] == 2){
+                swap(nums, i, right);
+                right--;
+            }
+            else{
+                i++;
+            }
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
