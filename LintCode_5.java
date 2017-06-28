eb87816a6bb7c608b63fc6128c1f148129732319
misspink1011 <xuanwang1118@gmail.com>
2017-06-28 16:59:53 -0400

    Kth Largest Element

---
+++ LintCode_5.java
@@ -0,0 +1,48 @@
+class LintCode_5 {
+    /*
+     * @param k : description of k
+     * @param nums : array of nums
+     * @return: description of return
+     */
+    public int kthLargestElement(int k, int[] nums) {
+        // write your code here
+        if(nums == null || nums.length == 0){
+            return -1;
+        }
+        
+        return quickSelect(nums, 0, nums.length - 1, k);
+    }
+    
+    private int quickSelect(int[] nums, int start, int end, int k){
+        if(start == end){
+            return nums[start];
+        }
+        
+        int left = start, right = end;
+        int pivot = nums[(start + end) / 2];
+        while(left <= right){
+            while(left <= right && nums[left] > pivot){
+                left++;
+            }
+            while(left <= right && nums[right] < pivot){
+                right--;
+            }
+            
+            if(left <= right){
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        
+        if(start + k - 1 <= right){
+            return quickSelect(nums, start, right, k);
+        }
+        if(start + k - 1 >= left){
+            return quickSelect(nums, left, end, k - (left - start));
+        }
+        return nums[right + 1];
+    }
+}
