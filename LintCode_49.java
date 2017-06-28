7d389262f3484e767ceba1b2bdf13336716d3fff
misspink1011 <xuanwang1118@gmail.com>
2017-06-28 17:55:56 -0400

    Sorted Letters by Case

---
+++ LintCode_49.java
@@ -0,0 +1,30 @@
+public class LintCode_49 {
+    /** 
+     *@param chars: The letter array you should sort by Case
+     *@return: void
+     */
+    public void sortLetters(char[] chars) {
+        //write your code here
+        if(chars == null || chars.length == 0){
+            return;
+        }
+        
+        int left = 0, right = chars.length - 1;
+        while(left <= right){
+            while(left <= right && chars[left] >= 'a' && chars[left] <= 'z'){
+                left++;
+            }
+            while(left <= right && chars[right] >= 'A' && chars[right] <= 'Z'){
+                right--;
+            }
+            
+            if(left <= right){
+                char temp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
