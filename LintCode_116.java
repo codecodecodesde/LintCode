ca2fe29c0d6497be1db9c018b813f8f3538b360b
misspink1011 <xuanwang1118@gmail.com>
2017-07-05 00:19:56 -0400

    Jump Game I

--- LintCode_116.java
+++ LintCode_116.java
@@ -18,3 +18,19 @@ public class LintCode_116 {
         return farthest >= A.length - 1;
     }
 }
+
+public class LintCode_116_2 {
+    public boolean canJump(int[] A) {
+        boolean[] can = new boolean[A.length];
+        can[0] = true;
+        for (int i = 1; i < A.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (can[j] && j + A[j] >= i) {
+                    can[i] = true;
+                    break;
+                }
+            }
+        }
+        return can[A.length - 1];
+    }
+}
