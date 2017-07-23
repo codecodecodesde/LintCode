c857ffd0075b8711c3e9f5be30edacc8d7152241
misspink1011 <xuanwang1118@gmail.com>
2017-07-23 20:07:40 +0800

    Min Stack

--- LintCode_12.java
+++ LintCode_12.java
@@ -29,3 +29,38 @@ public class MinStack {
         return minstack.peek();
     }
 }
+
+public class MinStack {
+    public Stack<Integer> stack;
+    public Stack<Integer> minstack;
+    public MinStack() {
+        // do initialize if necessary
+        stack = new Stack<>();
+        minstack = new Stack<>();
+    }
+
+    public void push(int number) {
+        // write your code here
+        stack.push(number);
+        if (minstack.isEmpty()) {
+            minstack.push(number);
+        } else {
+            if(minstack.peek() >= number){
+                minstack.push(number);
+            }
+        }
+
+    }
+
+    public int pop() {
+        // write your code here
+        if(stack.peek().equals(minstack.peek()))
+            minstack.pop();
+       return stack.pop();
+    }
+
+    public int min() {
+        // write your code here
+        return minstack.peek();
+    }
+}
