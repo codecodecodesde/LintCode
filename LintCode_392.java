b0aff476faecf35a97cc184a04c2d32290c88063
misspink1011 <xuanwang1118@gmail.com>
2017-07-30 16:02:33 +0800

    House Robber

---
+++ LintCode_392.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public long houseRobber(int[] A) {
+        // write your code here
+        int n = A.length;
+        if(n == 0)
+            return 0;
+        long []res = new long[2];
+        res[0] = 0;
+        res[1] = A[0];
+        for(int i = 2; i <= n; i++) {
+            res[i%2] = Math.max(res[(i-1)%2], res[(i-2)%2] + A[i-1]);
+        }
+    }
+}
